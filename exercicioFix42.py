9c6ed1037c402b8f2eb27506188b97072f0826bb
Ramonlegend <ramongodinho01@gmail.com>
2024-04-23 08:09:49 -0300

    programa 2

---
+++ exercicioFix42.py
@@ -0,0 +1,19 @@
+# RA: 1051392411014
+# Nome aluno: Ramon Godinho
+# Data: 06/04/2024
+# Descrição: Calcula a média de um aluno e verifica se ele foi aprovado ou não.
+print("RA: 1051392411014, Nome: Ramon Godinho, Turma: 1 semestre DSM ")
+
+nome = input("Digite seu nome: ")
+ra = input("Digite seu RA: ")
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+
+# Calcula a média do aluno
+media = (nota1 + nota2) / 2
+
+# Verifica se a média é maior ou igual a sete
+if media >= 7:
+  print("Parabéns, você está aprovado!")
+else:
+  print("Você ainda tem uma chance! Estude mais para o exame")
