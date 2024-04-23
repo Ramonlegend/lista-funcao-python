8591600b4b6ec6fb34a8d1e4a011e69933b6c988
Ramonlegend <ramongodinho01@gmail.com>
2024-04-23 09:50:30 -0300

    refatorado codigo 3

--- exercicioFix43.py
+++ exercicioFix43.py
@@ -1,22 +1,24 @@
 # RA: 1051392411014
 # Nome aluno: Ramon Godinho
 # Data: 23/04/2024
-# Descrição: Calcula a média de um aluno e verifica se ele foi aprovado ou não.
+# Descrição: Calcula a média de um aluno e verifica se ele foi aprovado ou não com o adicional do exame.
 print("RA: 1051392411014, Nome: Ramon Godinho, Turma: 1 semestre DSM ")
 
-# Solicita a entrada das notas
-nota1 = float(input("Digite a primeira nota: "))
-nota2 = float(input("Digite a segunda nota: "))
-notaExame = float(input("Digite a nota do exame: "))
+def calculate_grade():
+    nota1 = float(input("Digite a primeira nota: "))
+    nota2 = float(input("Digite a segunda nota: "))
+    notaExame = float(input("Digite a nota do exame: "))
 
-# Calcula a média aritmética
-media = (nota1 + nota2) / 2
+    # Calcula a média aritmética
+    media = (nota1 + nota2) / 2
 
-# Calcula a média final considerando a nota do exame
-mediaFinal = (media + notaExame) / 2
+    # Calcula a média final considerando a nota do exame
+    mediaFinal = (media + notaExame) / 2
 
-# Verifica se o aluno foi aprovado ou não
-if mediaFinal >= 5:
-  print("Parabéns, você aproveitou a sua chance! Está aprovado.")
-else:
-  print("Estude mais para a próxima. Você não alcançou o mínimo necessário.")
+    # Verifica se o aluno foi aprovado ou não
+    if mediaFinal >= 5:
+        print("Parabéns, você aproveitou a sua chance! Está aprovado.")
+    else:
+        print("Estude mais para a próxima. Você não alcançou o mínimo necessário.")
+
+calculate_grade()
